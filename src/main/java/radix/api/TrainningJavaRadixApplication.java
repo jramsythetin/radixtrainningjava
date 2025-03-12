e10c32fcbf80f492b4798f7d3dcdfb03e20965cb
Jram <jramsythetin@gmail.com>
2025-03-12 10:04:20 -0300

    Melhorias

---
+++ src/main/java/radix/api/TrainningJavaRadixApplication.java
@@ -0,0 +1,14 @@
+package radix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+	TrainningJavaRadixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningJavaRadixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/radix/api/TrainningJavaRadixApplicationTests.java
@@ -0,0 +1,13 @@
+package radix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainningJavaRadixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
